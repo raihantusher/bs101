fd00877e8811520de1d7fdb9fc23e633019a99ba
Raihan Ahmed <raihan.tusher@yahoo.com>
2021-01-30 18:37:29 +0600

    no message

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,12 +29,13 @@ class Database extends \CodeIgniter\Database\Config
 	 *
 	 * @var array
 	 */
+	//mysql://b901fa552df464:6197ea6b@us-cdbr-east-03.cleardb.com/heroku_7940434b28d4668?reconnect=true
 	public $default = [
 		'DSN'      => '',
-		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => '',
- 		'database' => 'candle',
+		'hostname' => 'us-cdbr-east-03.cleardb.com',
+		'username' => 'b901fa552df464',
+		'password' => '6197ea6b',
+		'database' => 'heroku_7940434b28d4668',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
