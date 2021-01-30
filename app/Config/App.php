8dc4c73b863dec31520bf5b16255f2c032cc7bd4
raihantusher <raihan.tusher@yahoo.com>
2021-01-30 13:32:00 +0600

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/bs101/public';
+	public $baseURL = 'https://bs101-dev.herokuapp.com/public/';
 
 	/*
 	|--------------------------------------------------------------------------
